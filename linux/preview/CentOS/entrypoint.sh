73db0cee8516b709714ac9ff520cde1a47c72c7b
jimgarrett123 <jimgarrett123@gmail.com>
2019-02-28 18:48:32 +0000

    update

--- linux/preview/CentOS/entrypoint.sh
+++ linux/preview/CentOS/entrypoint.sh
@@ -1,2 +1,2 @@
 #start SQL Server, start the script to create the DB and import the data, start the app
-sqlservr & sleep 90s & /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'RedHat!23' -d master -i /usr/sql/bin/setup.sql && /opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/sql/bin/Products.csv" -c -t',' -S localhost -U sa -P 'RedHat!23'
+/opt/mssql/bin/sqlservr & /usr/src/app/import-data.sh
--- linux/preview/CentOS/import-data.sh
+++ linux/preview/CentOS/import-data.sh
@@ -2,7 +2,7 @@
 sleep 90s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'RedHat!23' -d master -i setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'RedHat!23' -d master -i /usr/sql/bin/setup.sql
 
 #import the data from the csv file
 /opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/sql/bin/Products.csv" -c -t',' -S localhost -U sa -P 'RedHat!23'
