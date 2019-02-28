2b235aa650da6f644a8aac90c04e6371e71f9a53
jimgarrett123 <jimgarrett123@gmail.com>
2019-02-28 21:08:20 +0000

    update

--- linux/preview/CentOS/import-data.sh
+++ linux/preview/CentOS/import-data.sh
@@ -1,8 +1,10 @@
 #wait for the SQL Server to come up
-sleep 90s
+sleep 30s
 
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'RedHat!23' -d master -i /usr/sql/bin/setup.sql
 
 #import the data from the csv file
 /opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/sql/bin/Products.csv" -c -t',' -S localhost -U sa -P 'RedHat!23'
+
+exit 0
