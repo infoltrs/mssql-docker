2c5dec30f7c2654ab7226ba090d5983dd40fcc52
jimgarrett123 <jimgarrett123@gmail.com>
2019-03-07 20:16:51 +0000

    update

--- linux/preview/CentOS/setup.sql
+++ linux/preview/CentOS/setup.sql
@@ -1,6 +1,10 @@
-CREATE DATABASE DemoData;
-GO
-USE DemoData;
-GO
-CREATE TABLE Products (ID int, ProductName nvarchar(max));
-GO
+CREATE DATABASE DemoData;
+GO
+USE DemoData;
+GO
+CREATE TABLE Products (ID int, ProductName nvarchar(max));
+GO
+insert into Products (ID, ProductName) values (1,"Car");
+insert into Products (ID, ProductName) values (2,"Truck");
+insert into Products (ID, ProductName) values (3,"Motorcycle");
+GO
