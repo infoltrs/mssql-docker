67f0e32e7d58a6c9b3ea4e2f980d768ff5efc510
jimgarrett123 <jimgarrett123@gmail.com>
2019-03-07 20:40:29 +0000

    update

--- linux/preview/CentOS/entrypoint.sh
+++ linux/preview/CentOS/entrypoint.sh
@@ -2,4 +2,4 @@
 
 #start SQL Server, start the script to create the DB and import the data, start the app
 #/opt/mssql/bin/sqlservr & /usr/sql/bin/import-data.sh & npm start
-./SqlCmdStartup.sh & /opt/mssql/bin/sqlservr
+/usr/sql/bin/SqlCmdStartup.sh & /opt/mssql/bin/sqlservr
