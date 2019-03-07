85eb7c5470b04ed1ad084de2dde5562cfa16b05f
jimgarrett123 <jimgarrett123@gmail.com>
2019-03-07 16:37:38 +0000

    update

--- linux/preview/CentOS/import-data.sh
+++ linux/preview/CentOS/import-data.sh
@@ -5,10 +5,10 @@ sleep 60s
 
 #run the setup script to create the DB and the schema in the DB
 echo "/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'RedHat!23' -d master -i /usr/sql/bin/setup.sql"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'RedHat!23' -d master -i /usr/sql/bin/setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'RedHat!23' -d master -i /usr/sql/bin/setup.sql -o /usr/sql/bin/setup.log
 
 #import the data from the csv file
 echo "/opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/sql/bin/Products.csv" -c -t',' -S localhost -U sa -P 'RedHat!23'"
-/opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/sql/bin/Products.csv" -c -t',' -S localhost -U sa -P 'RedHat!23'
+/opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/sql/bin/Products.csv" -c -t',' -S localhost -U sa -P 'RedHat!23' -e /usr/sql/bin/bcp-error.log
 
 exit 0
